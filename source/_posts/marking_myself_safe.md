bdeb86ae5957c1083295d6354bdd77bb40d231ca
danielgrosvenor <daniel.t.grosvenor@gmail.com>
2019-12-09 20:55:09 +1300

    Close #2

---
+++ source/_posts/marking_myself_safe.md
@@ -0,0 +1,15 @@
+---
+extends: _layouts.post
+section: content
+title: We Are Safe
+date: 2019-12-7
+description: A quick post to let everyone know we are safe
+featured: true
+cover_image: /assets/images/jens-johnsson-qFYBki6u3Ik-unsplash.jpg
+cover_attribution: Photo by <a href="https://unsplash.com/@jens_johnsson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jens Johnsson on Unsplash</a>
+categories: [new zealand]
+---
+
+At around 2:00 PM in White Island in the Bay of Plenty off the coast of the North Island, there was a volcanic eruption that was more powerful than any of the eruptions in the past 60 years. There were an unknown number of injuries. I was 150 miles away from the eruption when it happened and so I was unaffected. I think if it wasn't a cloudy day then I would have been able to see the volcanic ash cloud from the top of Mount Victoria. It is just blending in with the other clouds. 
+
+Tomorrow I will read news reports and other information about the volcano and write a longer post about it. 
--- source/about.blade.php
+++ source/about.blade.php
@@ -8,15 +8,10 @@
 @endpush
 
 @section('body')
-    <h1>About</h1>
 
-    <img src="/assets/img/about.png"
+    <img src="/assets/images/daniel.jpg"
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6">I'm in 8th grade at Olympia Middle School in Stanford Illinois and I like cars. I started this blog as part of a homework assignment while traveling with my family across New Zealand.
 @endsection
